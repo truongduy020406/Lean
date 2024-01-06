f8a32ca842518e9f025c99ee5ca6352cc6004b7e
truongduy020406 <votruongduya2@gmail.com>
2024-01-06 20:41:27 +0700

    first commit

---
+++ array.cs
@@ -0,0 +1,19 @@
+arr = [ 1 ,3,2,5]
+
+arr.lenght: độ dài mảng
+arr.rank: chiều của mảng //mảng 1 chiều
+arr.min()
+arr.max()
+arr.sum()
+
+
+// mảng 2 chiều
+khai báo double[,] number = new double[2,3] {{2,3,4},{1,2,3}}
+/*  
+    2 3 4
+    1 2 3
+*/
+
+int hang = 2;
+int cot = 3;
+    
---
+++ string.cs
@@ -0,0 +1,13 @@
+string name = " duy be"
+
+//cắt khoản trắng 
+name.trim();
+
+name.trimStart() // cat vi tri dau
+name.trimEnd() // cat vi tri cuoi
+name.replace('be' , 'vo')  // thay be bang vo
+name.subString() // lay ra chuoi con
+name.remove(2 , 3) // xoa tu vi tri 2 3 ki tu
+name.split(' ') // chia chuoi ra 
+
+
